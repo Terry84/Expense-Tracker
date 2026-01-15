07ae3f85ba93755ef41b83027d67466ca31fd069
Terry84 <terrydaniels4184@gmail.com>
2026-01-15 14:11:35 +0300

    Update files with latest changes

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "expense-tracker-v1";
+
+const urlsToCache = [
+  "/",
+  "/static/style.css",
+  "/static/script.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
